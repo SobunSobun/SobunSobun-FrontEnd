34cf850c310035ca4a05b8b07c21cc7d919b9975
Cho Yunji <favorcho@betree.co.kr>
2022-11-07 17:58:29 +0900

    feat: lodash 추가

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,31 @@
+import { useState, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
+import { debounce } from 'lodash'
 
 import GNB from '../GNB'
 import styles from './layout.module.scss'
 
 const Layout = () => {
+  const [innerHeight, setInnerHeight] = useState<number>()
+  const handleResize = debounce(() => {
+    console.log(`브라우저 화면 사이즈 y:${window.innerHeight}`)
+    setInnerHeight(window.innerHeight)
+  }, 1000)
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
     <main className={styles.layout}>
       <div className={styles.container}>
         <div className={styles.inner}>
+          <p>
+            브라우저 화면사이즈 x: {window.innerWidth}, y: {window.innerHeight}
+          </p>
           <Outlet />
         </div>
         <GNB />
