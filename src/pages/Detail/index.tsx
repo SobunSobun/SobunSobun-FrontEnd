0182fedaa0517a302f7713bae04079fb19ccd2af
Jooseulgi <iauuai21@gmail.com>
2022-11-16 23:43:14 +0900

    feat: detail 페이지 레이아웃 작업

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Like, LikeOn } from 'assets/svgs'
+import { LikeIcon, LikeOnIcon } from 'assets/svgs'
 import Button from 'components/Button'
 import Comment from 'components/Detail/Comment'
 import DetailContent from 'components/Detail/DetailContent'
@@ -7,21 +7,10 @@ import Header from 'components/Header'
 import styles from './detail.module.scss'
 
 const Detail = () => {
-  const [isLike, setIsLike] = useState<boolean>(false)
-  const likeToggle = () => {
-    setIsLike(!isLike)
-  }
   return (
     <div className={styles.detail}>
       <div className={styles.headerWrap}>
-        <Header
-          leftChild={<Button type='back' />}
-          rightChild={
-            <div className={styles.like} onClick={likeToggle} role='presentation'>
-              {isLike ? <LikeOn /> : <Like />}
-            </div>
-          }
-        />
+        <Header leftChild={<Button type='back' />} rightChild={<div className={styles.edit}>수정</div>} />
       </div>
       <div className='contentsInner'>
         <DetailContent />
