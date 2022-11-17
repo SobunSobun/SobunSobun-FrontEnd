ee8417768c83ffb656f8eafb14a1d252dc3f8efa
Cho Yunji <favorcho@betree.co.kr>
2022-11-17 18:00:38 +0900

    feat: 지도 띄우는 함수 수정 및 swiper 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,9 +1,11 @@
 import fruits1 from 'assets/images/fruits_1.svg'
 import fruits2 from 'assets/images/fruits_2.svg'
 import fruits3 from 'assets/images/fruits_3.svg'
+import mapMarker from 'assets/images/map_marker.svg'
 
 export const IMAGE_PATH = {
   fruits1,
   fruits2,
   fruits3,
+  mapMarker,
 } as const
--- src/pages/Posting/Map/index.tsx
+++ src/pages/Posting/Map/index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import { UseGeoLocation } from 'hooks/useGeoLocation'
-import { Keyword, MapDataType, PaginationType, PlaceType } from 'types'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { IMAGE_PATH } from 'assets/images'
+import { Keyword, MapDataType, PlaceType } from 'types'
+import 'swiper/swiper.min.css'
 import styles from './map.module.scss'
 
 declare global {
@@ -9,232 +12,124 @@ declare global {
   }
 }
 
+const markerSrc = IMAGE_PATH.mapMarker
+const markerSize = new window.kakao.maps.Size(14, 20)
+const markerImage = new window.kakao.maps.MarkerImage(markerSrc, markerSize)
+
 const Map = ({ searchKeyword }: Keyword) => {
-  let markers: any[] = []
-  const { lat, lng } = UseGeoLocation()
+  // const markers: any[] = []
+  // const { lat, lng } = UseGeoLocation()
+  // useEffect(() => {
+  //   const container = document.getElementById('map')
+  //   const options = {
+  //     center: new window.kakao.maps.LatLng(lat, lng), // 지도의 중심좌표
+  //     level: 3, // 지도의 확대 레벨
+  //   }
 
-  // 검색어가 바뀔 때마다 재렌더링되도록 useEffect 사용
-  useEffect(() => {
-    const mapContainer = document.getElementById('map')
-    const mapOption = {
-      center: new window.kakao.maps.LatLng(lat, lng), // 지도의 중심좌표
-      level: 3, // 지도의 확대 레벨
-    }
+  //   const map = new window.kakao.maps.Map(container, options) // 지도 생성 및 객체 리턴
+  //   const ps = new window.kakao.maps.services.Places(map)
 
-    // 지도를 생성
-    const map = new window.kakao.maps.Map(mapContainer, mapOption)
+  //   /* 카테고리 검색 성공 callback */
+  //   const categoryCallback = (data: Array<MapDataType>, status: string) => {
+  //     if (status === window.kakao.maps.services.Status.OK) {
+  //       data.forEach((el: MapDataType) => {
+  //         const marker = new window.kakao.maps.Marker({
+  //           position: new window.kakao.maps.LatLng(el.y, el.x),
+  //           image: markerImage,
+  //         })
+  //         marker.setMap(map)
+  //       })
+  //     }
+  //   }
+  //   /* 마트 키워드로 검색한 결과 */
+  //   const keywordCallback = (data: Array<MapDataType>, status: string) => {
+  //     if (status === window.kakao.maps.services.Status.OK) {
+  //       data.forEach((el: MapDataType) => {
+  //         const marker = new window.kakao.maps.Marker({
+  //           position: new window.kakao.maps.LatLng(el.y, el.x),
+  //           image: markerImage,
+  //         })
+  //         marker.setMap(map)
+  //       })
 
-    // 장소 검색 객체를 생성
-    const ps = new window.kakao.maps.services.Places()
+  //       const moveLatLon = new window.kakao.maps.LatLng(data[0].y, data[0].x)
+  //       map.panTo(moveLatLon)
+  //     }
+  //   }
 
-    // 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성
-    const infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 })
-
-    // 키워드로 장소를 검색합니다
-    searchPlaces()
-
-    // 키워드 검색을 요청하는 함수
-
-    function searchPlaces() {
-      const keyword = searchKeyword
-
-      if (!keyword.replace(/^\s+|\s+$/g, '')) {
-        console.log('키워드를 입력해주세요!')
-        return
-      }
-
-      // 장소검색 객체를 통해 키워드로 장소검색을 요청
-      ps.keywordSearch(keyword, placesSearchCB)
-    }
-
-    // 장소검색이 완료됐을 때 호출되는 콜백함수
-    function placesSearchCB(data: Array<MapDataType>, status: string, pagination: PaginationType) {
-      if (status === window.kakao.maps.services.Status.OK) {
-        // 정상적으로 검색이 완료됐으면
-        // 검색 목록과 마커를 표출
-        displayPlaces(data)
-        console.log(data, status, pagination)
-
-        // 페이지 번호를 표출
-        displayPagination(pagination)
-      } else if (status === window.kakao.maps.services.Status.ZERO_RESULT) {
-        alert('검색 결과가 존재하지 않습니다.')
-      } else if (status === window.kakao.maps.services.Status.ERROR) {
-        alert('검색 결과 중 오류가 발생했습니다.')
-      }
-    }
-
-    // 검색 결과 목록과 마커를 표출하는 함수
-    // function displayPlaces(places: string | any[]) {
-    function displayPlaces(places: Array<MapDataType>) {
-      const listEl = document.getElementById('placesList')
-      const resultEl = document.getElementById('search-result')
-      const fragment = document.createDocumentFragment()
-      const bounds = new window.kakao.maps.LatLngBounds()
-
-      // 검색 결과 목록에 추가된 항목들을 제거
-      listEl && removeAllChildNods(listEl)
-
-      // 지도에 표시되고 있는 마커를 제거
-      removeMarker()
-
-      for (let i = 0; i < places.length; i += 1) {
-        // 마커를 생성하고 지도에 표시
-        const placePosition = new window.kakao.maps.LatLng(places[i].y, places[i].x)
-        const marker = addMarker(placePosition, i, undefined)
-        const itemEl = getListItem(i, places[i]) // 검색 결과 항목 Element를 생성
-
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-        // LatLngBounds 객체에 좌표를 추가
-        // eslint-disable-next-line prettier/prettier
-        bounds.extend(placePosition)
-        ;((_marker: any, title: string) => {
-          window.kakao.maps.event.addListener(_marker, 'mouseover', () => {
-            displayInfowindow(_marker, title)
-          })
-
-          window.kakao.maps.event.addListener(_marker, 'mouseout', () => {
-            infowindow.close()
-          })
-
-          itemEl.onmouseover = () => {
-            displayInfowindow(_marker, title)
-          }
-
-          itemEl.onmouseout = () => {
-            infowindow.close()
-          }
-        })(marker, places[i].place_name)
-
-        fragment.appendChild(itemEl)
-      }
-
-      // 검색결과 항목들을 검색결과 목록 Element에 추가
-      listEl && listEl.appendChild(fragment)
-      if (resultEl) {
-        resultEl.scrollTop = 0
-      }
-
-      // 검색된 장소 위치를 기준으로 지도 범위를 재설정
-      map.setBounds(bounds)
-    }
-
-    // 검색결과 항목을 Element로 반환하는 함수
-    function getListItem(index: number, places: PlaceType) {
-      const el = document.createElement('li')
-      const itemStr = `
-          <div class="info">
-            <span class="marker marker_${index + 1}">
-              ${index + 1}
-            </span>
-            <a href="${places.place_url}">
-              <h5 class="info-item place-name">${places.place_name}</h5>
-              ${
-                places.road_address_name
-                  ? `<span class="info-item road-address-name">
-                    ${places.road_address_name}
-                   </span>
-                   <span class="info-item address-name">
-                 	 ${places.address_name}
-               	   </span>`
-                  : `<span class="info-item address-name">
-             	     ${places.address_name}
-                  </span>`
-              }
-            </a>
-          </div>
-          `
-
-      el.innerHTML = itemStr
-      el.className = 'item'
-
-      return el
-    }
-
-    // 마커를 생성하고 지도 위에 마커를 표시하는 함수
-    function addMarker(position: any, idx: number, title: undefined) {
-      const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png' // 마커 이미지 url, 스프라이트 이미지
-      const imageSize = new window.kakao.maps.Size(36, 37) // 마커 이미지의 크기
-      const imgOptions = {
-        spriteSize: new window.kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
-        spriteOrigin: new window.kakao.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
-        offset: new window.kakao.maps.Point(13, 37), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
-      }
-      const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions)
-      const marker = new window.kakao.maps.Marker({
-        position, // 마커의 위치
-        image: markerImage,
-      })
-
-      marker.setMap(map) // 지도 위에 마커를 표출
-      markers.push(marker) // 배열에 생성된 마커를 추가
-
-      return marker
-    }
-
-    // 지도 위에 표시되고 있는 마커를 모두 제거합니다
-    function removeMarker() {
-      for (let i = 0; i < markers.length; i += 1) {
-        markers[i].setMap(null)
-      }
-      markers = []
-    }
-
-    // 검색결과 목록 하단에 페이지번호를 표시는 함수
-    function displayPagination(pagination: { last: number; current: number; gotoPage: (arg0: number) => void }) {
-      const paginationEl = document.getElementById('pagination') as HTMLElement
-      const fragment = document.createDocumentFragment()
-      let z
-
-      // 기존에 추가된 페이지번호를 삭제
-      while (paginationEl.hasChildNodes()) {
-        paginationEl.lastChild && paginationEl.removeChild(paginationEl.lastChild)
-      }
-
-      for (z = 1; z <= pagination.last; z += 1) {
-        const el = document.createElement('a') as HTMLAnchorElement
-        el.href = '#'
-        el.innerHTML = z.toString()
-
-        if (z === pagination.current) {
-          el.className = 'on'
-        } else {
-          el.onclick = ((k) => {
-            return () => {
-              pagination.gotoPage(k)
-            }
-          })(z)
-        }
-
-        fragment.appendChild(el)
-      }
-      paginationEl.appendChild(fragment)
-    }
-
-    // 검색결과 목록 또는 마커를 클릭했을 때 호출되는 함수
-    // 인포윈도우에 장소명을 표시
-    function displayInfowindow(marker: any, title: string) {
-      const content = `<div style="padding:5px;z-index:1;" class="marker-title">${title}</div>`
-
-      infowindow.setContent(content)
-      infowindow.open(map, marker)
-    }
-
-    // 검색결과 목록의 자식 Element를 제거하는 함수
-    function removeAllChildNods(el: HTMLElement) {
-      while (el.hasChildNodes()) {
-        el.lastChild && el.removeChild(el.lastChild)
-      }
-    }
-  }, [searchKeyword])
+  //   if (searchKeyword) {
+  //     ps.categorySearch('MT1', categoryCallback, {
+  //       location: map.getCenter(),
+  //     })
+  //     ps.keywordSearch(searchKeyword, keywordCallback, {
+  //       location: map.getCenter(),
+  //     })
+  //   }
+  // }, [lat, lng, searchKeyword])
   return (
     <div className={styles.map}>
-      <div id='map' style={{ width: '100vw', height: '50vh' }} />
-      <div id='search-result'>
-        <div className='scroll-wrapper'>
-          <ul id='placesList' />
+      <div id='map' className={styles.mapArea} />
+      <div className={styles.searchResult}>
+        <div className={styles.wrapper}>
+          <Swiper
+            spaceBetween={12}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide>
+              <button
+                type='button'
+                className={styles.searchItem}
+                onClick={() => {
+                  console.log('클릭')
+                }}
+              >
+                <span className={styles.marketName}>서초구 할인마트</span>
+                <span className={styles.marketAddress}>서울 서초구 매헌로 16 (양재동)</span>
+              </button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <button
+                type='button'
+                className={styles.searchItem}
+                onClick={() => {
+                  console.log('클릭')
+                }}
+              >
+                <span className={styles.marketName}>서초구 할인마트</span>
+                <span className={styles.marketAddress}>서울 서초구 매헌로 16 (양재동)</span>
+              </button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <button
+                type='button'
+                className={styles.searchItem}
+                onClick={() => {
+                  console.log('클릭')
+                }}
+              >
+                <span className={styles.marketName}>서초구 할인마트</span>
+                <span className={styles.marketAddress}>서울 서초구 매헌로 16 (양재동)</span>
+              </button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <button
+                type='button'
+                className={styles.searchItem}
+                onClick={() => {
+                  console.log('클릭')
+                }}
+              >
+                <span className={styles.marketName}>서초구 할인마트</span>
+                <span className={styles.marketAddress}>서울 서초구 매헌로 16 (양재동)</span>
+              </button>
+            </SwiperSlide>
+          </Swiper>
+          {/* <ul className={styles.list}>
+            <li />
+          </ul> */}
         </div>
-        <div id='pagination' />
       </div>
     </div>
   )
