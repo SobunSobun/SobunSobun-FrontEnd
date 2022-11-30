a66c89d354822b01e9ded0ef78d40251b478e491
yunji-cho <pplusmessage@gmail.com>
2022-12-01 01:05:41 +0900

    fix : profile 로직 수정 및 gif 교체, css 수정

---
+++ src/hooks/useProfile.ts
@@ -0,0 +1,48 @@
+import { useMutation, useQueryClient } from 'react-query'
+
+import { isAxiosError } from 'utils/axios'
+import { profileImageAPI, profileNameAPI } from 'apis/profile'
+
+const useProfile = () => {
+  const queryClient = useQueryClient()
+
+  const updatePageData = () => {
+    queryClient.invalidateQueries('myInfo')
+    queryClient.invalidateQueries('feedList')
+    queryClient.invalidateQueries({
+      queryKey: ['myPost'],
+      refetchInactive: true,
+    })
+    queryClient.invalidateQueries({
+      queryKey: ['LikeList'],
+      refetchInactive: true,
+    })
+  }
+
+  const imageMutate = useMutation(profileImageAPI, {
+    onSuccess() {
+      updatePageData()
+    },
+    onError(err) {
+      if (isAxiosError(err)) {
+        // eslint-disable-next-line no-console, no-alert
+        alert('파일 업로드가 실패하였습니다. 파일을 확인해주세요')
+      }
+    },
+  })
+  const nickNameMutate = useMutation(profileNameAPI, {
+    onSuccess() {
+      updatePageData()
+    },
+    onError(err) {
+      if (isAxiosError(err)) {
+        // eslint-disable-next-line no-console, no-alert
+        alert('닉네임 변경이 실패하였습니다. 다시 시도해주세요')
+      }
+    },
+  })
+
+  return { imageMutate, nickNameMutate }
+}
+
+export default useProfile
